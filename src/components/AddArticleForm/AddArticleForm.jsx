22eb6fe83824e9c924fdfd3a55d880ea3e0ebe85
Kamajor <kamajorqa@gmail.com>
2023-02-23 23:46:18 +0300

    Добавлена стилизация формы добавления новости. Описана логика авторасширения textarea при вводе текста.

--- src/components/AddArticleForm/AddArticleForm.jsx
+++ src/components/AddArticleForm/AddArticleForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import s from './addArticleForm.module.css';
 
 function AddArticleForm() {
   const [formData, setFormData] = useState({
@@ -7,6 +8,8 @@ function AddArticleForm() {
     image: '',
     tags: [],
   });
+
+  const [textAreaHeight, setTextAreaHeight] = useState('50');
 
   function handleFormSubmit(event) {
     event.preventDefault();
@@ -19,6 +22,14 @@ function AddArticleForm() {
     setFormData({ ...formData, [event.target.name]: event.target.value });
   }
 
+  function handleTextareaChange(event) {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+
+    !!event.target.value
+      ? setTextAreaHeight(event.target.scrollHeight)
+      : setTextAreaHeight(50);
+  }
+
   function handleSelectChange(event) {
     setFormData({ ...formData, [event.target.name]: event.target.value });
     console.log('event', event);
@@ -27,50 +38,53 @@ function AddArticleForm() {
   }
 
   return (
-    <>
-      <h1>Содержимое новой статьи</h1>
-      <form onSubmit={handleFormSubmit}>
-        <label>
-          Article title:
-          <input
-            type="text"
-            value={formData.title}
-            onChange={handleInputChange}
-            name="title"
-          />
-        </label>
-        <label>
-          Article content:
-          <textarea
-            value={formData.text}
-            onChange={handleInputChange}
-            name="text"
-          />
-        </label>
-        <label>
-          Image link:
-          <input
-            type="text"
-            value={formData.image}
-            onChange={handleInputChange}
-            name="image"
-          />
-        </label>
-        <label>
-          Выберите теги заметки:
-          <select
-            // value={[]}
-            multiple={true}
-            onChange={handleSelectChange}
-            name="tags"
-          >
-            <option value="новости">Новости</option>
-            <option value="истории">Истории</option>
-          </select>
-        </label>
+    <article className={s.container}>
+      <h1 className={s.title}>Содержание новой статьи</h1>
+      <form onSubmit={handleFormSubmit} className={s.form}>
+        <label htmlFor="newArticle-title">Заголовок статьи *</label>
+        <input
+          id="newArticle-title"
+          type="text"
+          value={formData.title}
+          onChange={handleInputChange}
+          name="title"
+          placeholder="Введите заголовок"
+          required
+        />
+        <label htmlFor="newArticle-text">Текст статьи *</label>
+        <textarea
+          id="newArticle-text"
+          value={formData.text}
+          onChange={handleTextareaChange}
+          name="text"
+          placeholder="Введите текст статьи"
+          required
+          style={{ height: `${textAreaHeight}px` }}
+        />
+        <label htmlFor="newArticle-imgLink">Ссылка на изображение</label>
+        <input
+          id="newArticle-imgLink"
+          type="text"
+          value={formData.image}
+          onChange={handleInputChange}
+          name="image"
+          placeholder="https://www.picture.jpg/"
+        />
+        <label htmlFor="newArticle-tags">Тег статьи</label>
+        <select
+          id="newArticle-tags"
+          // value={[]}
+          // multiple={true}
+          onChange={handleSelectChange}
+          name="tags"
+        >
+          <option value=""></option>
+          <option value="новости">Новости</option>
+          <option value="истории">Истории</option>
+        </select>
         <button type="submit">Опубликовать статью</button>
       </form>
-    </>
+    </article>
   );
 }
 
